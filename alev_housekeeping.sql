08d3e90b37dc645300ccf2c835704b123fa87655
mineroller <85659501+mineroller@users.noreply.github.com>
2022-10-20 14:31:08 +0800

    Add files via upload

--- alev_housekeeping.sql
+++ alev_housekeeping.sql
@@ -1,14 +1,20 @@
-DECLARE @OldAudit_Rows INT, @ToDel_Rows INT;
+DECLARE @OldAudit_Rows INT, @ToDel_Rows INT, @Older_Than INT;
 SET @OldAudit_Rows = 1;
 
-SELECT @ToDel_Rows = (SELECT count(*) from TestEntry WHERE ActionDateLocal < DATEADD(DAY,-1,GETDATE()));
+-------------------------------------------------------------
+-- Change this value to set different retention.
+-- MUST be in Negative number (i.e. -31 = older than 31 days)
+SET @Older_Than = -31;
+-------------------------------------------------------------
 
-PRINT 'For today (' + CAST(SYSDATETIME() AS VARCHAR) + ') there are '+ CAST(@ToDel_Rows AS VARCHAR) +' to delete. Starting Delete.';
+SELECT @ToDel_Rows = (SELECT count(*) from AuditEntry WHERE ActionDateLocal < DATEADD(DAY,@Older_Than,GETDATE()));
+
+PRINT 'For today (' + CAST(SYSDATETIME() AS VARCHAR) + ') there are '+ CAST(@ToDel_Rows AS VARCHAR) +' records older than '+ CAST(@Older_Than AS VARCHAR) + ' days to delete. Starting Delete.';
 
 WHILE (@OldAudit_Rows > 0)
 	BEGIN
 		BEGIN TRANSACTION
-		DELETE TOP(10000) FROM TestEntry WHERE ActionDateLocal < DATEADD(DAY,-1,GETDATE())
+		DELETE TOP(10000) FROM AuditEntry WHERE ActionDateLocal < DATEADD(DAY,@Older_Than,GETDATE())
 
 		SET @OldAudit_Rows = @@ROWCOUNT;
 
